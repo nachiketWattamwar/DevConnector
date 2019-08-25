cad8f7bc6f76faa21201f95ab93089e09c5a6e5c
nachiketWattamwar <nachiket.wattamwar@sjsu.edu>
2019-08-24 19:42:40 -0700

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-
+const path = require("path");
 //connect db
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("API running ");
-});
+// app.get("/", (req, res) => {
+//   res.send("API running ");
+// });
 
 //init middleware
 app.use(express.json({ extended: false }));
@@ -18,6 +18,15 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+//serve static assets in prod
+if (process.env.NODE_ENV) {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
