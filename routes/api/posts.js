a6fed9c333ca170f5cca7013a98f03b21243375e
nachiketWattamwar <nachiket.wattamwar@sjsu.edu>
2019-08-27 09:33:48 -0700

    api for liking a post by id implemented

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -63,4 +63,35 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
+//@route PUT api/posts/like/:id
+//@access private
+//@desc   Like a post
+router.put("/like/:id", auth, async (req, res) => {
+  //res.send("inside posts router ");
+
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(404).json("Post not found  ");
+    }
+
+    //check if the post is already liked
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: "Post already liked." });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+
+    res.json(post.likes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("server error");
+  }
+});
+
 module.exports = router;
