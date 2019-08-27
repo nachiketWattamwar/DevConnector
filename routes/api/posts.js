951ac609b4b5d24f6c636f0a77182bf76143374c
nachiketWattamwar <nachiket.wattamwar@sjsu.edu>
2019-08-27 09:49:07 -0700

    unlike api implemented

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -82,10 +82,43 @@ router.put("/like/:id", auth, async (req, res) => {
       post.likes.filter(like => like.user.toString() === req.user.id).length > 0
     ) {
       return res.status(400).json({ msg: "Post already liked." });
+    } else {
+      post.likes.unshift({ user: req.user.id });
+      await post.save();
     }
 
-    post.likes.unshift({ user: req.user.id });
-    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("server error");
+  }
+});
+
+//@route PUT api/posts/unlike/:id
+//@access private
+//@desc   unlike a post
+router.put("/unlike/:id", auth, async (req, res) => {
+  //res.send("inside posts router ");
+
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(404).json("Post not found.");
+    }
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: "Post is not liked yet." });
+    } else {
+      const removeIndex = post.likes
+        .map(like => like.user.toString())
+        .indexOf(req.user.id);
+      post.likes.splice(removeIndex, 1);
+      await post.save();
+    }
 
     res.json(post.likes);
   } catch (error) {
