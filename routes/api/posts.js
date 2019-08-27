bd9b8cc3061f476e55a178d0d3d2fcefeb208c75
nachiketWattamwar <nachiket.wattamwar@sjsu.edu>
2019-08-26 21:12:48 -0700

    api for creating post implemented

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+const Post = require("../../models/Post");
 
-//@route GET api/posts
-//@access public
-//@desc   Test route
-router.get("/", (req, res) => {
-  res.send("inside posts router ");
+//@route POST api/posts
+//@access private
+//@desc   create a new post
+router.post("/", auth, async (req, res) => {
+  //res.send("inside posts router ");
+
+  const user = await User.findById(req.user.id).select("-password");
+  try {
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    });
+
+    const post = await newPost.save();
+    res.send(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("server error");
+  }
 });
 
 module.exports = router;
